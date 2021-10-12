4095d182c3952393659a15cbd30eed3eb2baf113
jailor123 <91145870+jailor123@users.noreply.github.com>
2021-10-12 21:28:32 +0530

    nth_harmonic.cs

---
+++ p4.cs
@@ -0,0 +1,28 @@
+using System;
+ 
+class number
+{
+    
+static double nthHarmonic(int N)
+{
+  
+    float harmonic = 1;
+ 
+ 
+    for (int i = 2; i <= N; i++)
+    {
+        harmonic += (float)1 / i;
+    }
+ 
+    return harmonic;
+}
+ 
+
+static public void Main ()
+{
+    int N = 8;
+     
+    Console.Write(nthHarmonic(N));
+}
+}
+ 
