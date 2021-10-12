85f3506ba76d98fa6a4fd9c1b77889801f301b29
jailor123 <91145870+jailor123@users.noreply.github.com>
2021-10-12 21:46:23 +0530

    evenodd.cs

---
+++ p8.cs
@@ -0,0 +1,15 @@
+using System;  
+public class Evenodd  
+{  
+    public static void Main()  
+    {
+    int num1, rem1;
+    Console.Write("Input an integer : ");
+    num= Convert.ToInt32(Console.ReadLine()); 
+    rem = num % 2;
+    if (rem1 == 0)
+ Console.WriteLine("num is an even integer",num1);
+    else
+ Console.WriteLine("num  is an odd integer",num1);
+}
+}
